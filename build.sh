c46df48040effdd46da3f7134e7f9ca91b02ee61
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-02-09 17:01:57 +0000

    Prepared docker-compose and build script for deployment

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 commit=$(git log -n 1 --oneline | sed 's/\(.......\).*/\1/')
 docker build -t andykuszyk/marmyandme:$commit . 
 docker push andykuszyk/marmyandme:$commit
+sed -i "s/\[version\]/$commit/g" docker-compose.yml
+scp -o "StrictHostKeyChecking no" ./docker-compose.yml $douser:$dopass@$doip:~/docker-compose.yml
+ssh -o "StrictHostKeyChecking no" $douser:$dopass@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
