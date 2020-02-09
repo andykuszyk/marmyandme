5b978626c4fa250f68168ea09b6cc65f9937e8ae
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-02-09 19:25:08 +0000

    Corrected arguments

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ commit=$(git log -n 1 --oneline | sed 's/\(.......\).*/\1/')
 docker build -t andykuszyk/marmyandme:$commit . 
 docker push andykuszyk/marmyandme:$commit
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
-scp -o -i id_rsa "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
-ssh -o -i id_rsa "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
