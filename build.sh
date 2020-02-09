f2a4c1d747d72b87a1a8de545d16b126b1390dca
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-02-09 19:21:58 +0000

    Updated to use ssh key

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ commit=$(git log -n 1 --oneline | sed 's/\(.......\).*/\1/')
 docker build -t andykuszyk/marmyandme:$commit . 
 docker push andykuszyk/marmyandme:$commit
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
-scp -o "StrictHostKeyChecking no" ./docker-compose.yml $douser:$dopass@$doip:~/docker-compose.yml
-ssh -o "StrictHostKeyChecking no" $douser:$dopass@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
+scp -o -i id_rsa "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
+ssh -o -i id_rsa "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
