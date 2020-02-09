2a9cb5fa275987678e9b5b92d77f9e74a5f59f40
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-02-09 16:53:36 +0000

    Corrected typo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 commit=$(git log -n 1 --oneline | sed 's/\(.......\).*/\1/')
 docker build -t andykuszyk/marmyandme:$commit . 
-docker push andykuszyk/marmyandme:$commit:
+docker push andykuszyk/marmyandme:$commit
