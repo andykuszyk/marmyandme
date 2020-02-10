26930aeb3ffb9df5257fd8334ac4f99beb51e23e
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-02-10 08:41:52 +0000

    Made docker compose work for deployment and locally

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 commit=$(git log -n 1 --oneline | sed 's/\(.......\).*/\1/')
 docker build -t andykuszyk/marmyandme:$commit . 
 docker push andykuszyk/marmyandme:$commit
-sed -i "s/\[version\]/$commit/g" docker-compose.yml
+sed -i "s/build: \./image: andykuszyk/marmyandme:$commit/g" docker-compose.yml
 scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
 ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
